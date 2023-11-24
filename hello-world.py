af626f06e0301b97d01abe44201a2cdef3211783
Nestor Portela <nportela29@itfip.edu.co>
2023-11-24 17:41:57 -0500

    Se agrega una suma en hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -1,1 +1,5 @@
-print("Hola mundo, este es nuestro primer github actions")
+print("Hola mundo, este es nuestro primer github actions con una suma en python")
+num1 = 50_200
+num2 = 300
+
+print("la suma es igual a: ", num1+num2)
